2cfb994df28b2f2e98432506f0bb13b079c8c9a0
Andrew Smith <848smith@gmail.com>
2020-02-02 11:08:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word(sentence)
-  phrase = ["Hello", "there, ", "and ", "how ", "are ", "you? "]
-  phrase.collect do |word|
-    word.reverse
-  end
-  phrase.join
+  array = sentence.split(" ")
+  array.collect do |word|
+    
 end
