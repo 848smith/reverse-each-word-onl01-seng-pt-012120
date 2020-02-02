53021ca1a67ea0989f72453d4cfa545c72959f42
Andrew Smith <848smith@gmail.com>
2020-02-02 10:00:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   phrase = ["Hello ", "there, ", "and ", "how ", "are ", "you?"]
   new_phrase = []
-  phrase.reverse_each do |word|
-    new_phrase << word
+  phrase.each do |word|
+    new_phrase << word.reverse
   end
   new_phrase.join
 end
